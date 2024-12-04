523b69d3ccb3c4b02db39d820b1f81db923034a6
Sathya <74515743+SathR12@users.noreply.github.com>
2024-12-03 20:04:01 -0500

    Update sign_detection.py

--- sign_detection.py
+++ sign_detection.py
@@ -30,3 +30,41 @@ def isOctagon(contour):
         return True
     
     return False
+
+def createMask(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    lower_red_1 = np.array([0, 100, 100])
+    upper_red_1 = np.array([10, 255, 255])
+    mask_red_1 = cv.inRange(hsv, lower_red_1, upper_red_1)
+
+    lower_red_2 = np.array([160, 100, 100])
+    upper_red_2 = np.array([179, 255, 255])
+    mask_red_2 = cv.inRange(hsv, lower_red_2, upper_red_2)
+    
+    mask_red = mask_red_1 + mask_red_2
+    return mask_red
+ 
+    
+
+def drawRect(mask, img, contour):
+    if isOctagon(contour) and cv.contourArea(contour) > 200 and extractText(mask):
+        x, y, w, h = cv.boundingRect(contour)
+        if 1.1 >= w / h >= .9:
+            cv.rectangle(img, (x, y), (x + w, w + h), (0, 255, 0), 3)
+        
+
+#indefinite loop for processing the live feed
+while True:
+    ret, frame = camera.read()
+    for contour in extractContours(createMask(frame)):
+        drawRect(createMask(frame), frame, contour)
+        
+    cv.imshow("frame", frame)
+    cv.imshow("mask", createMask(frame))
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+    
+
+camera.release()
+cv.destroyAllWindows()
